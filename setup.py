7450dee0041400b1bd155354ba770bec5f1591db
shrunbr <shrunbr@as211553.net>
2021-04-25 00:58:08 -0500

    updated version to 0.2.0, changed author_email, added url

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='throne',
-    version='0.1.0',
+    version='0.2.0',
     author='shrunbr',
-    author_email='shrunbr@as211553.net',
+    author_email='shrunbr@throne.dev',
+    url='https://github.com/throne/throne-cli/',
     description='Watch the internet from up on your throne.',
     packages=['bin', 'src', 'src.parsers'],
     include_package_data=True,

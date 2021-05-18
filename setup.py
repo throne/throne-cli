aab6fce64c3608fdaa676b8731326ae221d2bc45
shrunbr <shrunbr@throne.dev>
2021-05-17 22:18:49 -0500

    updates setup.py and adds workflow for pypi

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
+import pathlib
 from setuptools import setup, find_packages
+
+parent_dir = pathlib.Path(__file__).parent
+README = (parent_dir / "README.md").read_text()
 
 setup(
     name='throne',
@@ -7,6 +11,9 @@ setup(
     author_email='shrunbr@throne.dev',
     url='https://github.com/throne/throne-cli/',
     description='Watch the internet from up on your throne.',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    license="BSD 3-Clause Clear License",
     packages=['bin', 'src', 'src.parsers'],
     include_package_data=True,
     install_requires=[

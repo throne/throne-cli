c73d3679fc315952277bc00f4b7dc86ef4360d06
shrunbr <shrunbr@throne.dev>
2023-03-08 10:29:10 -0600

    removes assert for exit code 0

--- tests/create_config.py
+++ tests/create_config.py
@@ -12,11 +12,9 @@ throne_pass = os.environ['THRONE_PASS']
 def test_throne_setapi():
     print("Testing: throne api set")
     response = runner.invoke(throne, ["api", "set", "-u", throne_user, "-p", throne_pass])
-    assert response.exit_code == 0
     assert "Successfully set throne API key." in response.output
 
 def test_shodan_setapi():
     print("Testing: throne shodan setapi")
     response = runner.invoke(throne, ["shodan", "setapi"], input=f"{shodan_key}")
-    assert response.exit_code == 0
     assert "Successfully set Shodan API key." in response.output

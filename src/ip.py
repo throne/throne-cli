93cb70e10cb201479ae5b0b63f81ae3c88b304f3
shrunbr <shrunbr@throne.dev>
2023-03-09 12:29:53 -0600

    rewrites info module to leverage throne API

--- src/ip.py
+++ src/ip.py
@@ -4,6 +4,8 @@
 # Import Third Party Modules
 import logging
 import click
+import os
+import yaml
 from requests import request
 # Import Throne Modules
 from src.parsers import json_request
@@ -13,11 +15,21 @@ from src.bgp import asn as get_asn
 # Set log variable for verbose output
 log = logging.getLogger(__name__)
 
+# Get home directory
+home = os.environ['HOME']
+config_file = f'{home}/.throne/config.yml'
+
+try:
+    config = yaml.safe_load(open(config_file))
+    throne_apikey = config['throne_key']
+except:
+    throne_apikey = None
+
 # ARIN BOOTSTRAP URL
 BOOTSTRAP_URL = 'https://rdap-bootstrap.arin.net/bootstrap'
 RIPENETINFO_URL = 'https://stat.ripe.net/data/network-info/data.json?resource='
 RIPEPREFIXOVER_URL = 'https://stat.ripe.net/data/prefix-overview/data.json?resource='
-MACADDRESS_URL = 'https://api.macvendors.com/'
+THRONE_API = 'https://api.throne.dev/'
 
 @click.group()
 def ip():
@@ -79,83 +91,51 @@ def info(ctx, ipaddress, all):
     """
     Retrieves IP and registered contact information.
     """
-    # URLs
-    bootstrap_url = '{0}/ip/{1}'.format(BOOTSTRAP_URL, ipaddress)
-    ripe_url = '{0}{1}'.format(RIPEPREFIXOVER_URL, ipaddress)
-    # Parse Responses
-    ## Bootstrap Response
-    response_bootstrap = json_request._JSONRequest().get_json(url=bootstrap_url)
-    bootstrap_json = response_bootstrap
-    parse_bootstrap_json = ip_parser._RDAPIPEntity(bootstrap_json)
-    parse_bootstrap_json.parse()
-    bootstrap_results = parse_bootstrap_json.vars
-    ## RIPE Response
-    ripe_response = json_request._JSONRequest().get_json(url=ripe_url)
-    ripe_json = ripe_response
-    parse_ripe_json = ip_parser._RIPEPrefixOverview(ripe_json)
-    parse_ripe_json.parse()
-    ripe_results = parse_ripe_json.vars
-    # Set Variables
-    rir = bootstrap_results['rir']
-    startAddress = bootstrap_results['startAddress']
-    endAddress = bootstrap_results['endAddress']
-    ipVersion = bootstrap_results['ipVersion']
-    name = bootstrap_results['name']
-    status = bootstrap_results['type']
-    handle = bootstrap_results['handle']
-    cidr = ripe_results['prefix']
-    if ripe_results['asns'] is None:
-        asnstr = "None"
-    else:
-        for asn in ripe_results['asns']:
-            asnstr = asn['asn']
-    announced = ripe_results['announced']
-    # Meat & Potatos (Start outputing to user)
-    click.secho("---IP Info---", fg='green')
-    if "RIPE" in bootstrap_results['rir']:
-        log.debug("RIPE detected as RIR. CIDR has been changed to 'Parent Handle'.")
-        click.echo(f"Issued By: {rir}\nStatus: {status}\nName: {name}\nParent Handle: {cidr}\n Announced: {announced}\n Announced By: {asnstr}\n Version: {ipVersion}\n Beginning: {startAddress}\n Ending: {endAddress}")
-    else:
-        click.echo(f"Issued By: {rir}\nStatus: {status}\nName: {name}\nCIDR: {cidr}\n Announced: {announced}\n Announced By: {asnstr}\n Version: {ipVersion}\n Beginning: {startAddress}\n Ending: {endAddress}")
-    click.secho(f'---{rir}/{handle} Contact Information---', fg='green')
-    if "RIPE" in rir:
-            log.debug("Detected RIPE as RIR...all non-abuse contacts are filtered by RIPE. See RIPE database docs for more information.")
-    for ent in bootstrap_results['entities']:
-        name = ent['name']
-        kind = ent['roles']
-        delimeter = "/"
-        if ent['roles'] is None:
-            pass
+    if throne_apikey:
+        # Throne API Query
+        throne_url = '{0}whois/ip?query={1}'.format(THRONE_API, ipaddress)
+        throne_headers = {'Authorization': f'{throne_apikey}'}
+        throne_response = request("GET", throne_url, headers=throne_headers)
+        throne_result = throne_response.json()
+        # RIPE API Query
+        ripe_url = '{0}{1}'.format(RIPEPREFIXOVER_URL, ipaddress)
+        ripe_response = request("GET", ripe_url)
+        ripe_result = ripe_response.json()
+        # Parsing responses
+        if ripe_result['data']['asns'] is None:
+            asnstr = "None"
         else:
-            kind = delimeter.join(kind).title()
-        click.secho(f'{name} ({kind}):')
-        try:
-            for email in ent['email']:
-                email = email['value']
-        except:
-            email = "None"
-        try:
-            for streetAddress in ent['address']:
-                streetAddress = streetAddress['value']
-        except:
-            streetAddress = "None"
-        try:
-            for phone in ent['phone']:
-                phone = phone['value']
-        except:
-            phone = "None"
-        if "RIPE" in rir:
-            click.echo(" Entity Address: " + streetAddress + "\n Entity Phone: " + phone + "\n Entity Email: " + email)
-            click.secho("\nSome of these details may be filtered by RIPE. To verify this information please visit https://apps.db.ripe.net/db-web-ui/query.", fg='red')
-        else:
-            click.echo(" Entity Address: " + streetAddress + "\n Entity Phone: " + phone + "\n Entity Email: " + email)
-    if all:
-        if ripe_results['asns'] is None:
-            click.secho("\nThis prefix appears to not be advertised. There are no related ASNs to get BGP info for.", fg='red')
-        # Otherwise send ASNs to asn command in bgp.py
-        else:
-            for asn in ripe_results['asns']:
+            for asn in ripe_result['data']['asns']:
                 asnstr = asn['asn']
-                ctx.invoke(get_asn, as_number=asnstr)
-    else:
-        pass
+                holderstr = asn['holder']
+        # Output to user
+        click.secho("---IP Info---", fg='green')
+        click.echo(f"RIR: {throne_result['rir']}\nIssued By: {ripe_result['data']['block']['desc']}\nName: {throne_result['name']}\n Announced: {ripe_result['data']['announced']}\n Announced By: {asnstr} / {holderstr}\n Version: {throne_result['ipVersion']}\n Beginning: {throne_result['startAddress']}\n Ending: {throne_result['endAddress']}")
+        click.secho(f'---{throne_result["rir"]}/{holderstr} Contact Information---', fg='green')
+        for ent in throne_result['entities']:
+            name = ent['name']
+            type = ent['roles']
+            delimeter = "/"
+            if ent['roles'] is None:
+                pass
+            else:
+                type = delimeter.join(type).title()
+            click.secho(f'{name} ({type}):')
+            if "RIPE" in throne_result['rir']:
+                click.echo(" Entity Address: " + ent['address'] + "\n Entity Phone: " + str(ent['phone']) + "\n Entity Email: " + ent['email'])
+                click.secho("\nSome of these details may be filtered by RIPE. To verify this information please visit https://apps.db.ripe.net/db-web-ui/query.", fg='red')
+            else:
+                click.echo(" Entity Address: " + ent['address'] + "\n Entity Phone: " + str(ent['phone']) + "\n Entity Email: " + ent['email'])
+        if all:
+            if ripe_result['data']['asns'] is None:
+                click.secho("\nThis prefix appears to not be advertised. There are no related ASNs to get BGP info for.", fg='red')
+            # Otherwise send ASNs to asn command in bgp.py
+            else:
+                for asn in ripe_result['data']['asns']:
+                    asnstr = asn['asn']
+                    ctx.invoke(get_asn, as_number=asnstr)
+        else:
+            pass
+    if throne_apikey is None:
+        click.secho("throne API key required! Run `throne api set` to configure your API key.", fg="red")
+        click.secho("If you do not have an account, please register for one by visting https://api.throne.dev/auth/login and click 'Sign Up' at the bottom of the prompt", fg="red")
